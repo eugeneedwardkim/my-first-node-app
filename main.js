4a6b8ca10aa11c52de37e4dcea56ed29cedc4103
Eugene Kim <eugene.edward.kim@gmail.com>
2017-01-31 20:46:01 -0700

    more error

--- main.js
+++ main.js
@@ -1,12 +1,22 @@
+//required
 var http = require('http');
 var fs = require('fs');
+var eE = require('events').EventEmitter;
+
+//global
 var port = 8080;
+var logger = new eE();
+
+logger.on('error', function(message) {
+console.log('ERROR: ' + message);
+});
 
 http.createServer( function(request, response){
     
     fs.readFile('index.html', function(err, content) {
         if(err) {
             response.writeHead(404, {'Content-Type': 'text/html'});
+            logger.emit('error', err);
             response.end('failed');
         } else {
             response.writeHead(200, {'Content-Type': 'text/html'});
